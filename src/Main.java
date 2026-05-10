21d2bbd99d2a5fd2ec799c52492ef8892f81f811
Bufka2011 <bufka.bulanov@gmail.com>
2026-05-09 21:32:04 -0600

    Multi-line comments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
-    // My first actual Java code, hooray!
-    // btw my first java comment
+    /*
+    My first actual Java code, hooray!
+    btw my first java comment
+     */
 
     // A magic spell that does no idea what
     public static void main(String[] args){
-        // My first program!
-        // Hello Java world!
+        /*
+        My first program!
+        Hello Java world!
+         */
         System.out.println("Hello World!");
         System.out.println("I am Bufka2011 and I am learning Java!");
     }
