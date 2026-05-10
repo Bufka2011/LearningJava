e02e733e9e52cb9dd98b6eeab1b24f1aa9278c46
Bufka2011 <bufka.bulanov@gmail.com>
2026-05-09 21:50:40 -0600

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
 public class Main {
-    /*
-    My first actual Java code, hooray!
-    btw my first java comment
-     */
+    // My first actual Java code, hooray!
+    // btw my first java comment
 
     // A magic spell that does no idea what
     public static void main(String[] args){
-        /*
-        My first program!
-        Hello Java world!
-         */
+
+        // My first program!
+        // Hello Java world!
         System.out.println("Hello World!");
-        System.out.println("I am Bufka2011 and I am learning Java!");
+        // We use + to append something inside print method
+        // For example:
+        // System.out.println(2 + 2);
+        System.out.println("I am Bufka2011 and" + "I am learning Java!");
+
+
+
+        // Variables
+        // Reusable containers for values.
+        // A variable behaves as if it was the value it contains.
+        // There are two data types in Java.
+
+        // PRIMITIVE - simple value stored directly in memory (stack)
+        int Integer = 15;
+        double Double = 29.03;
+        char Character = 'A';         // <--- SINGLE QUOTES (') ONLY!
+        boolean TrueOrFalse = true;
+        // byte, short, long, float, double, void and more...
+
+        // REFERENCE - memory address (stack) that points to the (heap)
+        String String = "String";
+        // object
+        // array
+
+        System.out.println("");
+        System.out.println("VARIABLES:");
+        System.out.println("The integer is " + Integer);
+        System.out.println("The double is " + Double);
+        System.out.println("The character is " + "\"" + Character + "\"");
+        System.out.println("The TrueOrFalse is " + TrueOrFalse);
+        System.out.println("The string is " + "\"" + String + "\"");
     }
 }
