1bbf153d9c2d818449ddf5a4aa7dbb1893ef899c
Bufka2011 <bufka.bulanov@gmail.com>
2026-05-09 21:28:24 -0600

    The magic spell

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
     // My first actual Java code, hooray!
     // btw my first java comment
+
+    // A magic spell that does no idea what
+    public static void main(String[] args){
+    }
 }
